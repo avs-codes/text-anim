61027e0edf38b708ab3a8c20384861ca065485b4
avs-codes <silva892@newschool.edu>
2023-11-17 17:36:42 -0500

    Idk what do anymore 🫡

--- text-shuffle.js
+++ text-shuffle.js
@@ -1,7 +1,13 @@
 const shuffleDivs = document.querySelectorAll('.text-shuffle');
 const spot = document.getElementById('spot');
-const emojis = ['█','▓','▒','░'];
-const lettersAndSymbols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#', '$', '&', '*', '(', ')', '-', '_', '+', '=', '/', '[', ']', '{', '}', ';', ':', '<', '>', ',', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const dotW = 90;
+const dotH = 90;
+spot.style.width = dotW + 'px';
+spot.style.height = dotH + 'px';
+// spot.style.backgroundColor = 'red'
+const shaded = [' █',' ▓',' ▒',' ░'];
+const blocks = [' &#9601; ', ' &#9602; ', ' &#9603; ', ' &#9604; ', ' &#9605; ', ' &#9606; ', ' &#9607; ', ' &#9608; ']
+const lettersAndSymbols = [' A', ' B', ' C', ' D', ' E', ' F', ' G', ' H', ' I', ' J', ' K', ' L', ' M', ' N', ' O', ' P', ' Q', ' R', ' S', ' T', ' U', ' V',  'W', ' X', ' Y', ' Z', ' 0', ' 1', ' 2', ' 3', ' 4', ' 5', ' 6', ' 7', ' 8', ' 9'];
 let textSpans;
 
 const pickRandFromArr = (array) => array[Math.floor(Math.random() * array.length)];
@@ -24,8 +30,9 @@ shuffleDivs.forEach((element) => {
 });
 
 window.addEventListener('mousemove', (evt) => {
-  spot.style.left = evt.pageX - 20 + 'px';
-  spot.style.top = evt.pageY - 20 + 'px';
+  spot.style.left = evt.pageX - (dotW / 2) + 'px';
+  spot.style.top = evt.pageY - (dotH / 2) + 'px';
+
 });
 
 const isColliding = (element1, element2) => {
@@ -40,19 +47,33 @@ const isColliding = (element1, element2) => {
   );
 };
 
+// textSpans.forEach((element) => {
+//   element.innerHTML = pickRandFromArr(blocks);
+
+//     if (isColliding(spot, element)) {
+//       element.innerText = element.dataset.original;
+//     }
+// })
+
 const redraw = () => {
   textSpans.forEach((element) => {
+    //element.innerHTML = pickRandFromArr(shaded);
     if (isColliding(spot, element)) {
-      element.innerText = pickRandFromArr(emojis);
+      element.innerHTML = pickRandFromArr(lettersAndSymbols);
     } else if (element.innerText !== element.dataset.original) {
-      element.innerText = element.dataset.original
+      element.innerHTML = element.dataset.original;
+
     }
+      // element.innerHTML = pickRandFromArr(shaded);
+    // } else if (element.innerText !== element.dataset.original) {
+    //   element.innerHTML = element.dataset.original;
+    // }
   });
 }
 let count = 0;
 const animate = () => {
   
-  if (count % 10 === 0) {
+  if (count % 2 === 0) {
     redraw();
   }
   count++
